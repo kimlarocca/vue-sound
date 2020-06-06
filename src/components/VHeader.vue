59a0213db02638f44207f940dbaeb0307e5f6a5f
Kim LaRocca <kim@kimlarocca.com>
2020-06-06 11:28:16 -0400

    initial commit

---
+++ src/components/VHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <div class="logo">
+      <img alt="black lives matter morristown nj" src="/img/logo.png"/>
+    </div>
+    <nav>
+      <i class="fas fa-bars"></i>
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'VHeader'
+  }
+</script>
+
+<style lang="scss">
+  header {
+    position: relative;
+    z-index: 100;
+  }
+
+  nav {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+
+    i {
+      color: black;
+      font-size: 2rem;
+    }
+  }
+
+  .logo {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: white;
+    width: 150px;
+    height: 175px;
+    display: flex;
+    padding: 1rem;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="home">
+    <div class="container">
+      <h1>Join the movement, fight for freedom</h1>
+      <a class="button arrow">take action</a>
+    </div>
+    <div class="hero"></div>
+  </section>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+<style lang="scss" scoped>
+  .home {
+    background: var(--color-primary);
+    min-height: calc(100vh - 10rem);
+    position: relative;
+    padding: 4rem 0 0 0;
   }
-}
-</script>
+
+  .hero {
+    z-index: 10;
+    position: absolute;
+    background: url(https://images.unsplash.com/photo-1591155426209-23831d47700d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80) no-repeat;
+    top: 4rem;
+    right: -3rem;
+    height: calc(100vh - 13rem);
+    left: 12rem;
+    width: 100vw;
+    background-size: cover;
+  }
+
+  .container {
+    margin-left: 8rem;
+  }
+
+  h1 {
+    position: relative;
+    z-index: 20;
+    transform: rotate(-5deg);
+    font-family: var(--font-family-rock-salt);
+    color: var(--color-white);
+    text-shadow: 0 0 20px var(--color-dark-gray);
+    font-size: 6rem;
+    line-height: 6rem;
+    margin-bottom: 7rem;
+  }
+
+  .button {
+    position: relative;
+    z-index: 30;
+  }
+</style>
