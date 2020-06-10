6cd8fad0ee9007b5f60e48b4ae8f21241eb0b303
Kim LaRocca <kim@kimlarocca.com>
2020-06-10 10:22:37 -0400

    added 404 page and updated to history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import NotFound from '../views/404.vue'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '*',
+    component: NotFound
+  },
   {
     path: '/',
     name: 'Home',
@@ -23,6 +28,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
---
+++ src/views/404.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="about">
+    <h1>404 not found</h1>
+  </main>
+</template>
