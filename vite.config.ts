9266805a90a8c9e26556ffb1ca3104c1dc6c08b0
Kim LaRocca <kim@kimlarocca.com>
2024-08-06 14:51:30 -0400

    update vite base for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/vue-sound/',
+  base: '/vue-sound/',
   plugins: [vue()],
   build: {
     lib: {
